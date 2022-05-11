e52e0a19429bf978a3a745758a1c9636ccb3277b
SeongWooSeok90 <101170781+SeongWooSeok90@users.noreply.github.com>
2022-05-11 20:09:42 +0900

    Initial commit

---
+++ src/abvc.java
@@ -0,0 +1,10 @@
+public class abvc {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("카운트 다운을 시작합니다.");
+        while (a>=0);
+        System.out.println(a);
+        a--;
+
+    }
+}
---
+++ src/asdsad.java
@@ -0,0 +1,38 @@
+//public class asdsad {
+   // public static void main(String[] args) {
+        //int a=1~100;
+       // int b=1~100;
+             // if (a<b){
+                 // System.out.println(">");
+           //   }
+              //else if (a>b){
+                  //System.out.println("<");
+           //   }
+             // else (){
+           // System.out.println("=");
+    //    }
+  //  }
+
+//public class asdsad {
+//    public static void main(String[] args) {
+//        int sum=0;
+ //       for (int i=1; i<=20; i++){
+ //           sum+=i;
+//        }
+ //       System.out.println(sum);
+
+
+
+//    }
+//}
+public class asdsad {
+    public static void main(String[] args) {
+        int a=2012;
+        if(a%4==0 && a%100!=0 || a%400==0) {
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+    }
+    }
